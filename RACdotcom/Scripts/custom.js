7acc8d124d09fa70da5b0bc288dd03a4e900722b
Ryan Compton <ryan.compton@ZIRMED.COM>
2017-02-14 12:39:38 -0500

    add necalli pic, more cleanup

--- RACdotcom/Scripts/custom.js
+++ RACdotcom/Scripts/custom.js
@@ -1,25 +1,24 @@
 ﻿var clickHelper = (function (jQuery) {
     var module = {
         onready: function () {
-            //alert("test");
             $('#resumeBtn').on("click", function () {
-                alert("resume click detected");
                 ga('send', 'event', 'resume');
             });
             $('#navToggle').on("click", function () {
                 var x = document.getElementById("customNav");
-                        if (x.className === "topnav") {
-                            x.className += " responsive";
-                        } else {
-                            x.className = "topnav";
-                        }
-                //alert("navToggle click detected");
-                //$('customNav.topnav').addClass("responsive");
-                //$('customNav.responsive').removeClass("responsive");
+                if (x.className === "topnav") {
+                    x.className += " responsive";
+                } else {
+                    x.className = "topnav";
+                }
+            });
+            $('#mypic').bind('mouseenter mouseleave', function () {
+                $(this).attr({
+                    src: $(this).attr('data-other-src')
+                    , 'data-other-src': $(this).attr('src')
+                })
             });
         }
     }//module
-
     return module;
-
 })(jQuery);
